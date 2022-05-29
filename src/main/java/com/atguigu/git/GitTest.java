a8ed9d87eefbb10ac0224cc46210e9518079d7ff
uestcxt <uestcxiatian@gmail.com>
2022-05-30 00:28:32 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
         System.out.println("Hello, Git again");
+        System.out.println("Hello, Git again and again");
     }
 }
